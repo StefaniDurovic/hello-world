0a478cf28f75a7b439f2d59a0b9031e99df081ed
StefaniDurovic <95530637+StefaniDurovic@users.noreply.github.com>
2023-01-16 13:50:45 +0100

    initiated the chat app, made the start screen

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TextInput, Button, Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// import react native gesture handler
+import 'react-native-gesture-handler';
+
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Start from './components/Start';
+import Chat from './components/Chat';
+
+// Create the navigator
+const Stack = createStackNavigator();
+
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Start">
+          <Stack.Screen name="Start" component={Start} />
+          <Stack.Screen name="Chat" component={Chat} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react';
+// import { StyleSheet, View, Text, TextInput, Button, Alert } from 'react-native';
+
+// export default class App extends React.Component {
+//  constructor(props) {
+//    super(props);
+//    this.state = { text: '' };
+//  }
+
+//  // alert the user input
+//  alertMyText (input = []) {
+//   Alert.alert(input.text);
+// }
+
+//  render() {
+//    return (
+//      <View style={{ flex: 1, justifyContent: "center" }}>
+//        <TextInput
+//          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+//          onChangeText={(text) => this.setState({ text })}
+//          value={this.state.text}
+//          placeholder="Type here ..."
+//        />
+//        <Text>You wrote: {this.state.text}</Text>
+//        <Button
+//          onPress={() => { this.alertMyText({ text: this.state.text }); }}
+//          title="Press Me"
+//        />
+//      </View>
+//    );
+//  }
+// }
---
+++ components/Chat.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+
+
+export default class Chat extends React.Component {
+
+    componentDidMount() {
+        let name = this.props.route.params.name;
+        this.props.navigation.setOptions({ title: name })
+      }
+
+  render() {
+    let color = this.props.route.params.color;
+    return (
+        <View style={[styles.container, { backgroundColor: color }]}>
+        <Text>Hello!</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+  })
